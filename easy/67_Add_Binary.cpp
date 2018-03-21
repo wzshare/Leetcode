9cd7f514fd7a4d6e113279eedde0484be1a4f7b9
wangzhe <wangzhe@baofeng.com>
2018-03-21 10:18:33 +0800

    short for Add Binary

--- easy/67_Add_Binary.cpp
+++ easy/67_Add_Binary.cpp
@@ -12,40 +12,18 @@ using namespace std;
 class Solution {
 public:
     string addBinary(string a, string b) {
-        if(a == "")
-            return b;
-        if(b == "")
-            return a;
-        int astr = a.size();
-        int bstr = b.size();
-        int carry = 0;//记录进位
-		int aint = 0, bint =0; //记录a、b的下标
-        string result = astr > bstr ? a : b;
-        int re = astr > bstr ? astr : bstr;
-        do
-        {           
-            if(astr > 0)
-                aint = a[astr - 1] - '0';
-            else
-                aint = 0;
-            if(bstr > 0)
-                bint = b[bstr - 1] - '0';
-            else
-                bint = 0;
-			int sum = aint + bint + carry;
-			if(re == 0)
-                result.insert(0, "1"); //首位进位采用在起始插入
-            else
-                result[re - 1] = sum % 2 + '0';
-            if(sum > 1)
-                carry = 1;
-            else
-                carry = 0;
-            astr --;
-            bstr --;
-            re --;
-        }while(carry != 0 || astr > 0 || bstr > 0);//所有位计算完且没有进位才结束
-        return result;
+        string s = "";
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int c = 0;
+        while (i >= 0 || j >= 0 || c == 1)
+        {
+            c += i >= 0 ? a[i--] - '0' : 0;
+            c += j >= 0 ? b[j--] = '0' : 0;
+            s = char(c % 2 + '0') + s;
+            c /= 2;
+        }
+        return s;
     }
 };
 
