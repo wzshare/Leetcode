cb37b5c781dffe1c42d976feb0e9251248fbcad5
wangzhe <angel-smiles@163.com>
2018-03-26 15:37:00 +0800

    add question 105

---
+++ medium/105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.cpp
@@ -0,0 +1,39 @@
+#include "../libstruct.h"
+#include <vector>
+
+using namespace std;
+
+/*
+ * NO.105
+ * Given preorder and inorder traversal of a tree, construct the binary tree.
+ */
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return buildtree(preorder, 0, preorder.size(), inorder, 0, inorder.size());
+    }
+    
+    TreeNode* buildtree(vector<int>& preorder, int i, int j, vector<int>& inorder, int ii, int jj)
+    {
+        if(i >= j || ii >= jj)
+            return NULL;
+        int mid = preorder[i];
+        auto f = find(inorder.begin() + ii, inorder.begin() + jj, mid);
+        int dis = f - inorder.begin() - ii;
+
+        TreeNode* root = new TreeNode(mid);
+        root -> left = buildtree(preorder, i + 1, i + 1 + dis, inorder, ii, ii + dis);
+        root -> right = buildtree(preorder, i + 1 + dis, j, inorder, ii + dis + 1, jj);
+        return root;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution solution;
+    vector<int> preorder = {3, 9, 20, 15, 7};
+    vector<int> inorder = {9, 3, 15, 20, 7};
+    solution.buildTree(preorder, inorder);
+    return 0;
+}
