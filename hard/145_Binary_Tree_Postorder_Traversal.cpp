32b7f58e8df64343f1df12a373dc7a2e48e59102
wangzhe <angel-smiles@163.com>
2018-03-29 15:44:39 +0800

    add question 145

--- hard/145_Binary_Tree_Postorder_Traversal.cpp
+++ hard/145_Binary_Tree_Postorder_Traversal.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 /*
- * NO.144
- * Given a binary tree, return the preorder traversal of its nodes' values.
- */ 
+ * NO.145
+ * Given a binary tree, return the postorder traversal of its nodes' values.
+ */
 
 class Solution {
 public:
     //Recursive
-    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> postorderTraversal(TreeNode* root) {
         vector<int> vec;
         helper(root, vec);
         return vec;
@@ -18,26 +18,26 @@ public:
 private:
     void helper(TreeNode* root, vector<int>& vec) {
         if (!root) return;
-        vec.push_back(root->val);
         helper(root->left, vec);
         helper(root->right, vec);
+        vec.push_back(root->val);
     }
 public:
     //Iterative
-    vector<int> preorderTraversal2(TreeNode* root) {
+    vector<int> postorderTraversal2(TreeNode* root) {
         vector<int> vec;
-        stack<TreeNode*> st;
-        while (root || !st.empty()) {
-            if (root) {
-                st.push(root);
-                vec.push_back(root->val);
-                root = root->left;
-            }
-            else {
-                root = st.top();
-                root = root->right;
-                st.pop();
-            }
+        stack<TreeNode*> st1, st2;
+        if (root) st1.push(root);
+        while (!st1.empty()) {
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            if (root->left) st1.push(root->left);
+            if (root->right) st1.push(root->right);
+        }
+        while (!st2.empty()) {
+            vec.push_back(st2.top()->val);
+            st2.pop();
         }
         return vec;
     }
@@ -45,7 +45,7 @@ public:
 
 /*
  * input: [1,2,3,#,4]
- * output: [1,2,4,3]
+ * output: [4,2,3,1]
  */
 TreeNode* testTree()
 {
@@ -59,11 +59,11 @@ TreeNode* testTree()
 int main(int argc, char const *argv[])
 {
     Solution solution;
-    vector<int> vec = solution.preorderTraversal(testTree());
+    vector<int> vec = solution.postorderTraversal(testTree());
     for (auto v : vec)
         cout << v << " ";
     cout << endl;
-    vec = solution.preorderTraversal2(testTree());
+    vec = solution.postorderTraversal2(testTree());
     for (auto v : vec)
         cout << v << " ";
     cout << endl;
