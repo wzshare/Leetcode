45e6c510d8cf514f7c1c0b769c72aa4021a74753
wzshare <angel-smiles@163.com>
2018-04-01 17:57:18 +0800

    modify

--- medium/8_String_to_Integer_(atoi).cpp
+++ medium/8_String_to_Integer_(atoi).cpp
@@ -5,6 +5,9 @@ using namespace std;
 /*
  * NO.8
  * Implement atoi to convert a string to an integer. 
+ * If no valid conversion could be performed, a zero value is returned. 
+ * If the correct value is out of the range of representable values, 
+ * INT_MAX (2147483647) or INT_MIN (-2147483648) is returned.
  */ 
 
 class Solution {
