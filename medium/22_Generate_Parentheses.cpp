33bc7a3e638b4b6104859e027c777caa4ed81c5d
wangzhe <angel-smiles@163.com>
2018-04-09 12:21:41 +0800

    modify

--- medium/22_Generate_Parentheses.cpp
+++ medium/22_Generate_Parentheses.cpp
@@ -22,6 +22,23 @@ public:
         if (l > 0) helper(vec, str + '(', l - 1, r + 1);
         if (r > 0) helper(vec, str + ')', l, r - 1);
     }
+
+    //DP solution
+    //dp[0] = ""
+    //dp[i]='('+ dp[k]+')'+dp[i-1-k],k=0..i-i
+    vector<string> generateParenthesis1(int n) {
+        vector< vector<string> > dp(n+1, vector<string>());
+        dp[0].push_back("");
+        for(int i=1; i<=n; ++i){
+            for(int k=0; k<i; ++k){
+                for(string s1: dp[k]){
+                    for(string s2: dp[i-1-k])
+                        dp[i].push_back("("+s1+")"+s2);
+                }
+            }
+        }
+        return dp[n];
+    }
 };
 /*
 given n = 3, a solution set is:
@@ -32,6 +49,9 @@ int main(int argc, char const *argv[])
 {
     Solution solution;
     vector<string> vec = solution.generateParenthesis(3);
+    for (auto v : vec) cout << v << " ";
+    cout << endl;
+    vec = solution.generateParenthesis1(3);
     for (auto v : vec) cout << v << " ";
     cout << endl;
     return 0;
