cd691dbdef64e62fc97ca58dda6f4c7c04f4a76e
wangzhe <angel-smiles@163.com>
2018-04-16 18:19:23 +0800

    add question 100

---
+++ easy/100_Same_Tree.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "../libstruct.h"
+
+using namespace std;
+
+/*
+ * No.100
+ * Given two binary trees, write a function to check if they are the same or not.
+ * Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+ */
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p || !q) return p == q;
+        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution solution;
+    return 0;
+}
