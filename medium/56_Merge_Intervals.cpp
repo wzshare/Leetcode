7fc397cacb240e6739a605b9b9c44e1e940efcaa
wangzhe <angel-smiles@163.com>
2018-04-24 17:44:52 +0800

    modify

--- medium/56_Merge_Intervals.cpp
+++ medium/56_Merge_Intervals.cpp
@@ -8,16 +8,14 @@ using namespace std;
 class Solution {
 public:
     vector<Interval> merge(vector<Interval>& intervals) {
-        if (intervals.empty()) return vector<Interval>{};
-        vector<Interval> res;
+        if (intervals.empty()) return intervals;
         sort(intervals.begin(), intervals.end(), [](Interval a, Interval b) {return a.start < b.start;});
-        res.push_back(intervals[0]);
+        vector<Interval> result{intervals[0]};
         for (int i = 1; i < intervals.size(); i++) {
-            if (res.back().end < intervals[i].start) res.push_back(intervals[i]);
-            else
-                res.back().end = max(res.back().end, intervals[i].end);
+            if (result.back().end < intervals[i].start) result.push_back(intervals[i]);
+            else result.back().end = max(result.back().end, intervals[i].end);
         }
-        return res;
+        return result;
     }
 };
 
