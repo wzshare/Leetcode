04a68cc0ba8848faa6acca18c2b7727605a5a0bc
wzshare <angel-smiles@163.com>
2018-04-24 21:38:18 +0800

    modify

--- medium/57_Insert_Interval.cpp
+++ medium/57_Insert_Interval.cpp
@@ -2,15 +2,28 @@
 
 using namespace std;
 
+/* 
+ * NO.57
+ * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+ * You may assume that the intervals were initially sorted according to their start times.
+ */
 class Solution {
 public:
     vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
         vector<Interval> result;
-        if (intervals.empty()) {
-            result.push_back(newInterval);
-            return result;
+        int index = 0;
+        while(index < intervals.size() && intervals[index].end < newInterval.start){
+            result.push_back(intervals[index++]);
         }
-        result.push_back(intervals[0]);
+        while(index < intervals.size() && intervals[index].start <= newInterval.end){
+            newInterval.start = min(newInterval.start, intervals[index].start);
+            newInterval.end = max(newInterval.end, intervals[index++].end);
+        }
+        result.push_back(newInterval);
+        while(index < intervals.size()){
+            result.push_back(intervals[index++]);
+        }
+        return result;
     }
 };
 
