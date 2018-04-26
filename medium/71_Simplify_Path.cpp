de1208f7cd0901272530f4fccc3aec054bd25b51
wangzhe <angel-smiles@163.com>
2018-04-26 17:35:00 +0800

    add question 71

---
+++ medium/71_Simplify_Path.cpp
@@ -0,0 +1,35 @@
+#include "../libstruct.h"
+
+using namespace std;
+
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string result;
+        stack<string> st;
+        for(int i = 0; i < path.length(); i++) {
+            string temp;
+            while(i < path.length() && path[i] != '/') temp += path[i++];
+            if (temp == "..") {
+                if (!st.empty()) st.pop();
+            }
+            else if (temp != "." && !temp.empty()) st.push(temp);
+        }
+        if (st.empty()) result = "/";
+        while (!st.empty()) {
+            string t = st.top();
+            st.pop();
+            result = "/" + t + result;
+        }
+        return result;
+    }
+};
+
+// input: "/a/./b/../../c"
+// out: "/c"
+int main(int argc, char const *argv[])
+{
+    Solution solution;
+    cout << solution.simplifyPath("/a/./b/../../c") << endl;
+    return 0;
+}
