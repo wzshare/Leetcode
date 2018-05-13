2a598075455d2b5defe56776d8a73816144d8b93
wzshare <angel-smiles@163.com>
2018-05-13 22:19:55 +0800

    Create 141_Linked_List_Cycle.cpp
    add question 141

---
+++ easy/141_Linked_List_Cycle.cpp
@@ -0,0 +1,28 @@
+#include "../libstruct.h"
+
+using namespace std;
+
+/*
+ * NO.141
+ * Given a linked list, determine if it has a cycle in it.
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head, *slow = head;
+        while(fast) {
+            fast = fast->next;
+            if (fast && (slow = slow->next)) fast = fast->next;
+            else return false;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution solution;
+    return 0;
+}
